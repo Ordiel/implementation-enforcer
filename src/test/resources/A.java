1cb1f54ca13f2136190fd33eec8be9f443d26c65
Ordiel <ordiel1910@gmail.com>
2015-09-21 18:33:23 -0500

    **src/test/resources/A.java & src/test/resources/B.java**
    * Classes useful to test the functionality of the library.

---
+++ src/test/resources/A.java
@@ -0,0 +1,10 @@
+import javax.lang.model.element.Modifier;
+
+import ordiel.enforcer.MethodRequester;
+
+import ordiel.enforcer.Types.Void;
+
+@MethodRequester(identifier = "aMethod", type = Void.class, parametersTypes={}, parametersIdentifiers = {}, requestedModifiers={Modifier.STATIC})
+public abstract class A {
+
+}
